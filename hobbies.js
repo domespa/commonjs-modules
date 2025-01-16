390e43e6e0c872adbd7e6ac3ec48765ecb8dcdc0
domespa <dumiii1988@gmail.com>
2025-01-16 15:00:43 +0100

    feat: create file hobbies

---
+++ hobbies.js
@@ -0,0 +1,8 @@
+// Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+function hobby(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+// Esporta la funzione dal file.
+
+module.exports = hobby 
