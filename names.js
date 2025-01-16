36c5e201183804d3b412a2f75f9ac86869baa08f
domespa <dumiii1988@gmail.com>
2025-01-16 14:48:58 +0100

    feat: create function name

---
+++ names.js
@@ -0,0 +1,11 @@
+// Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+function name(firstName, lastName) {
+    return {
+        firstName: firstName,
+        lastName: lastName
+    };
+}
+
+// Esporta la funzione dal file.
+
+module.exports = name
